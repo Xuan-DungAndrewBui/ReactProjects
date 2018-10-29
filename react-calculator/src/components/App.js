eeaa1a1b31d387ad75b69c7ae87c5c191b7b030c
Andrew Bui <andrewbui101112@gmail.com>
2018-10-29 12:03:01 +1100

    Added basic operations
    Implemented four main operations as well as equals, clear all and sign swapping

--- react-calculator/src/components/App.js
+++ react-calculator/src/components/App.js
@@ -2,32 +2,111 @@ import React from 'react';
 import Display from './display';
 import Key from './key';
 
+const calcExpression = {
+    '+': function(firstValue, secondValue ) { return firstValue+secondValue },
+    '-': function(firstValue, secondValue ) { return firstValue-secondValue },
+    '*': function(firstValue, secondValue ) { return firstValue*secondValue },
+    "/": function(firstValue, secondValue ) { return firstValue/secondValue },
+    "=": function(firstValue, secondValue ) { return secondValue}
+};
+
 //App takes care of logic
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            operations: [],
-            currentValue: 0,
+            operator: '',
+            displayValue: '0',
+            firstValue: null,
+            secondValue: 0,
+            needOperator: true, 
         }
     }
 
+    // performOperation() {
+    //     const firstValue = this.state.firstValue;
+        
+    //     if (firstValue === null) {
+    //         this.setState({
+
+    //         })
+    //     }
+
+    // }
+
+    setOperation(operator) {
+        const firstValue = this.state.firstValue;
+        const value = parseInt(this.state.displayValue);
+
+        if (firstValue === null) {
+            this.setState({
+                firstValue: value,
+            });
+        } else if (this.state.operator)  {
+                const result = calcExpression[this.state.operator](firstValue,value);
+                this.setState({
+                    firstValue: result,
+                    displayValue: String(result),
+                });
+            }
+
+        this.setState({
+            operator: operator,
+            needOperator: true
+        });
+        } 
+
+    setEquals() {
+        const operator = this.state.operator;
+        const firstValue = this.state.firstValue;
+        const value = parseInt(this.state.displayValue);
+        const result = calcExpression[operator](firstValue,value);
+        this.setState({
+            firstValue: result,
+            displayValue: String(result),
+            needOperator: true,
+            operator: '='
+        });
+    }
     //Basic functionality of inputting a number into the display
     addInput(number) {
-        const addition = String(this.state.currentValue) + String(number);
-        const combine = parseInt(addition);
+        const needOperator = this.state.needOperator;
+        if (needOperator) {
         this.setState({
-            currentValue: combine,
+            displayValue: String(number),
+            needOperator: false
+        });
+        } else {
+            const combine = parseFloat(String(this.state.displayValue) + String(number));
+            this.setState({
+                displayValue: combine.toString(),
+            });
+        }            
+    }
+    
+    clearAll() {
+        this.setState({
+            operator: '',
+            displayValue: '0',
+            firstValue: null,
+            secondValue: 0,
+            needOperator: true, 
+        })
+    }
+    
+    changeSign() {
+        this.setState({
+            displayValue: this.state.displayValue * -1
         })
     }
 
     //Break the calculator into display and keys
     render() {
-        const currentValue = this.state.currentValue
+        const displayValue = this.state.displayValue;
         return (
             <div>    
                 <div>
-                    <Display output={currentValue} />
+                    <Display output={displayValue} />
                 </div>
                 <div className = 'numberpad'>
                     <Key value={'1'} className='calculator-key' onClick={() => this.addInput(1)} />
@@ -39,6 +118,17 @@ class App extends React.Component {
                     <Key value={'7'} className='calculator-key' onClick={() => this.addInput(7)} />
                     <Key value={'8'} className='calculator-key' onClick={() => this.addInput(8)} />
                     <Key value={'9'} className='calculator-key' onClick={() => this.addInput(9)} />
+                    <Key value={'0'} className='calculator-key' onClick={() => this.addInput(0)} />
+                </div>
+                <div className='calculator-operations'>
+                    <Key value={'+'} className='calculator-key' onClick={() => this.setOperation('+')}/>
+                    <Key value={'-'} className='calculator-key' onClick={() => this.setOperation('-')}/>
+                    <Key value={'x'} className='calculator-key' onClick={() => this.setOperation('*')}/>
+                    <Key value={'÷'} className='calculator-key' onClick={() => this.setOperation('/')}/>
+                    <Key value={'='} className='calculator-key' onClick={() => this.setOperation('=')}/>
+                    <Key value={'C'} className='calculator-key' onClick={() => this.clearAll()}/>
+                    <Key value={'+/-'} className='calculator-key' onClick={() => this.changeSign()}/>
+
                 </div>
             </div>
         )
